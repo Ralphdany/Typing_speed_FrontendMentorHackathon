91fc07105ade664501fcac8a4e88df61c3360e62
Ralphdany <ralphnjambou@gmail.com>
2025-12-30 20:44:29 +0100

    chore: images provider added to avoid multiple image imports

---
+++ src/constants/images.ts
@@ -0,0 +1,23 @@
+import logoLarge from '../assets/images/logo-large.svg';
+import logoSmall from '../assets/images/logo-small.svg';
+import trophey from '../assets/images/icon-personal-best.svg';
+import iconCompleted from '../assets/images/icon-completed.svg';
+import iconRestart from '../assets/images/icon-restart.svg';
+import arrowDown from '../assets/images/icon-down-arrow.svg';
+import cheerIcon from '../assets/images/icon-new-pb.svg';
+import patternConfetti from '../assets/images/pattern-confetti.svg';
+import patternStar1 from '../assets/images/pattern-star-1.svg';
+import patternStar2 from '../assets/images/pattern-star-2.svg';
+
+export const IMAGES = {
+  logoLarge,
+  logoSmall,
+  trophey,
+  iconCompleted,
+  iconRestart,
+  arrowDown,
+  cheerIcon,
+  patternConfetti,
+  patternStar1,
+  patternStar2,
+};
